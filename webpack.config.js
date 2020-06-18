bd4fbdee345319c889c3a40edb874a82f8c1d963
Tianhao Zheng <347111927@qq.com>
2020-06-18 18:55:19 +0800

    增加配置

--- webpack.config.js
+++ webpack.config.js
@@ -17,11 +17,17 @@ module.exports = function (env, argv) {
                 exclude: /node_modules/,
                 use: ["babel-loader"]
             },
-            // {
-            //     test: /\.css$/,
-            //     include: [path.resolve(__dirname, 'src/styles'), /node_modules/],
-            //     use: ["style-loader", "css-loader", "postcss-loader"]
-            // }
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ["file-loader"]
+            },
+            {
+                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/],
+                loader: "url-loader",
+                options: {
+                    limit: 10000
+                }
+            }
             ]
         }
     };
